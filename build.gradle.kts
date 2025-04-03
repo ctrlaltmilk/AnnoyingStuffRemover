998d8bf0e8ecbffe342b29afd4b8617b1f7932ed
ctrlaltmilk <iris@ctrlaltmilk.net>
2025-04-02 19:25:49 -0700

    Change loader version requirement (fixes #1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 val modId = "asr"
 
-project.version = "1.0.0"
+project.version = "1.0.1"
 project.group = "net.ctrlaltmilk"
 
 java.toolchain.languageVersion = JavaLanguageVersion.of(17)
